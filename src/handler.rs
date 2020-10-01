7d9621140f0fcfd80a06c91db0583b395432a386
Joseph Zurowski <Dunklheit@users.noreply.github.com>
2020-10-01 19:05:40 -0400

    Debug

--- src/handler.rs
+++ src/handler.rs
@@ -66,6 +66,7 @@ impl EventHandler for Handler {
 
                   match manager.get_mut(&guild) {
                     Some(handler) => {
+                      println!("Starting YouTube download...");
                       let source = match voice::ytdl("https://youtube.com/watch?v=S7rM1zmCj1M").await {
                         Ok(source) => source,
                         Err(why) => {
